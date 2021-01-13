0e28c72c8d8614618f1f0ebf05e2b90a11429d4b
Jie Zheng <prettykingking@live.com>
2021-01-13 23:27:44 +0800

    initial commit

---
+++ init.el
@@ -0,0 +1,10 @@
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; change command to meta, and ignore option to use weird Norwegian keyboard
+(setq mac-option-modifier 'super)
+(setq mac-command-modifier 'meta)
+(setq ns-function-modifier 'hyper)
+
